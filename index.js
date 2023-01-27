b28884067651fa8215f0f6018e3a6c389dbd2a5f
Andres Felipe Caro <andresfelipe.caro@rockwellautomation.com>
2023-01-27 18:41:42 -0500

    upload first version and modules of test for the library

---
+++ index.js
@@ -0,0 +1,57 @@
+module.exports = {
+
+    /**
+     * Suma de dos numeros
+     * @example
+     * n1 = 1, n2 = 2 , resultado = 3
+     * @param {*} n1 numero uno de la suma
+     * @param {*} n2 numero dos de la suma
+     * @returns  suma de dos numeros
+     */
+
+    suma: function(n1,n2){
+        return n1+n2;
+    },
+
+      /**
+       * resta de dos numeros
+     * @example
+     * n1 = 1, n2 = 2 , resultado = -1
+     * @param {*} n1 numero uno de la resta
+     * @param {*} n2 numero dos de la resta
+     * @returns  resta de dos numeros
+     */
+
+    resta: function(n1,n2){
+        return n1-n2;
+    },
+
+      /**
+     * multiplicacion de dos numeros
+     * @example
+     * n1 = 1, n2 = 2 , resultado = 2
+     * @param {*} n1 numero uno de la multiplicacion
+     * @param {*} n2 numero dos de la multiplicacion
+     * @returns  multiplicacion de dos numeros
+     */
+
+    multiplicar: function(n1,n2){
+        return n1*n2;
+    },
+
+      /**
+       * division de dos numeros
+     * @example
+     * n1 = 1, n2 = 2 , resultado = 0.5
+     * @param {*} n1 numero uno de la division 
+     * @param {*} n2 numero dos de la division 
+     * @returns   division de dos numeros
+     */
+
+    dividir: function(n1,n2){
+        return n1/n2;
+    }
+
+
+
+}
---
+++ pruebas.js
@@ -0,0 +1,3 @@
+const m = require('.');
+
+console.log(m.suma(2,2));
